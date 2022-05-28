d6c31e9a441a69c118eaf8c70c7a05db4bf3dbfd
bbbozhena <bbbozhenka@gmail.com>
2022-05-28 22:00:21 +0300

    first generator

---
+++ Generator.js
@@ -0,0 +1,19 @@
+
+const quote = document.getElementById ("quote");
+const author = document.getElementById ("author");
+const btn = document.getElementById ("btn");
+
+const url = "https://api.quotable.io/random";
+
+
+let getQuote = () => {
+    fetch (url)
+    .then((data) => data.json())
+    .then ((item) => {
+        quote.innerHTML = item.content;
+        author.innerHTML = item.author;
+    });
+};
+
+window.addEventListener ('load', getQuote);
+btn.addEventListener('click', getQuote);
